f2da8bcd036df888260d944847078f639723cadf
Gabriel Sinapsis <gabyreload@gmail.com>
2023-05-05 15:53:31 -0300

    adding code_technical in UsersDto

--- src/dao/DTOs/users.dto.js
+++ src/dao/DTOs/users.dto.js
@@ -3,6 +3,7 @@ export default class UsersDto {
     (this.first_name = user.first_name),
       (this.last_name = user.last_name),
       (this.email = user.email),
+      (this.code_technical = user.code_technical),
       (this.adminView = user.adminView),
       (this.role = user.role);
   }
