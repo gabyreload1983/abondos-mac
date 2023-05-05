8c527a5c42cecfac15f4146f173bc1d7523f79fc
Gabriel Sinapsis <gabyreload@gmail.com>
2023-05-05 14:55:10 -0300

    autenticatetion and authtoritation users router

--- src/routes/api/users.router.js
+++ src/routes/api/users.router.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { login, logout, register } from "../../controllers/users.controller.js";
+import { passportCall } from "../../utils.js";
+import { authorization } from "../../utils.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  passportCall("jwt"),
+  authorization("superAdmin"),
+  register
+);
 
 router.post("/login", login);
 
-router.get("/logout", logout);
+router.get("/logout", passportCall("jwt"), logout);
 
 export default router;
